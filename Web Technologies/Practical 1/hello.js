6203820b1aa7ee24974327930725fd6159ae6a03
Akai <rishabh.hs@somaiya.edu>
2025-09-17 19:37:29 +0530

    feat: Add basic HTTP server and math module with tests
    Introduced hello.js with a simple HTTP server and two exported functions for addition and subtraction. Added test.js to demonstrate usage of these functions.

---
+++ Web Technologies/Practical 1/hello.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var port = 3000;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(port);
+console.log('Server running at http://localhost:' + port + '/');
+
+module.exports = {
+    addNumbers : function(a, b) {
+        return a + b;
+    },
+    subtractNumbers : function(a, b) {
+        return a - b;
+    }
+}
---
+++ Web Technologies/Practical 1/test.js
@@ -0,0 +1,4 @@
+var test = require('./hello.js');
+
+console.log("Addition of 5 and 3: " + test.addNumbers(5, 3));
+console.log("Subtraction of 5 from 10: " + test.subtractNumbers(10, 5));
