c0d71aebaa44d18896a3cf510a1f599362077323
Akai <rishabh.hs@somaiya.edu>
2026-01-29 00:45:40 +0530

    Add query to view specific partition in Transaction1
    Added a SELECT statement to retrieve data from partition p1 of the Transaction1 table, demonstrating how to query a specific partition.

--- DBMS/Partitioning.sql
+++ DBMS/Partitioning.sql
@@ -9,6 +9,8 @@ PARTITION BY RANGE(cust_id) (
 	PARTITION p3 VALUES LESS THAN (MAXVALUE)
 ); -- creates 3 partitions based on cust_id ranges
 
+SELECT * FROM Transaction1 PARTITION (p1); -- to view partition p1
+
 INSERT INTO Transaction1 VALUES (101, 1, TO_DATE('2023-10-21', 'YYYY-MM-DD'), 1000); -- goes to p1
 INSERT INTO Transaction1 VALUES (104, 2, TO_DATE('2023-10-22', 'YYYY-MM-DD'), 2000); -- goes to p2
 INSERT INTO Transaction1 VALUES (106, 3, TO_DATE('2023-10-23', 'YYYY-MM-DD'), 3000); -- goes to p3
